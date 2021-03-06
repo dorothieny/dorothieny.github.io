e81185267ab59236144d4e0d40fda846d1c43917
dorothieny <74118617+dorotheny@users.noreply.github.com>
2021-03-06 22:25:14 +0300

    new kode more work today hard

---
+++ cinema/kode.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".chel").click(function(){
+      $(".chel").toggleClass("sit");
+    });
+    // $(".fi").click(function(){
+    //     $(".chel.sit").addClass("stand");
+    //     $(".chel.sit").removeClass("sit")
+    //     setTimeout(function(){
+    //       $(".chel.stand").removeClass("stand");
+    //     }, 1200);
+    //   });
+    
+});
