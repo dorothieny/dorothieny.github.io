613e84d6ead7f720af84d3bf3b591174f416fa7b
dorothieny <74118617+dorotheny@users.noreply.github.com>
2021-03-07 18:09:16 +0300

    slider finally shit

--- cinema/kode.js
+++ cinema/kode.js
@@ -29,3 +29,50 @@ $(document).ready(function(){
           }, 3500);
     });
 });
+
+$(document).ready(function(){
+var currentPosition = 0;
+var slideWidth = 560;
+var slides = $('.slide');
+var numberOfSlides = slides.length;
+
+$('#slidesContainer').css('overflow', 'hidden');
+
+slides
+.wrapAll('<div id="slideInner"></div>')
+
+.css({
+'float' : 'left',
+'width' : slideWidth
+});
+
+$('#slideInner').css('width', slideWidth * numberOfSlides);
+
+$('#slideshow')
+.prepend('<span class="control" id="leftControl">Move left</span>')
+.append('<span class="control" id="rightControl">Move right</span>');
+
+manageControls(currentPosition);
+
+$('.control')
+.bind('click', function(){
+
+currentPosition = ($(this).attr('id')=='rightControl')
+? currentPosition+1 : currentPosition-1;
+
+manageControls(currentPosition);
+
+$('#slideInner').animate({
+  'marginLeft' : slideWidth*(-currentPosition)
+});
+});
+
+function manageControls(position){
+
+if(position==0){ $('#leftControl').hide() }
+else{ $('#leftControl').show() }
+
+if(position==numberOfSlides-1){ $('#rightControl').hide() }
+else{ $('#rightControl').show() }
+}
+});
