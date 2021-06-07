d45b2d0515803e0b0b7a29cacb5d067e15fa89fa
dorothieny <dorothinein@gmail.com>
2021-06-07 17:44:35 +0300

    ups

--- omyt/special.js
+++ omyt/special.js
@@ -4,16 +4,18 @@ $(document).ready(function(){
     $('.specialfirst').toggleClass('hidden')
   });
 
-  $('.b2').click(function(){
-    $('.b2').toggleClass('domiche');
 
-    // $('.specialfirst').toggleClass('hidden')
-  });
 
 
   $('.b3').click(function(){
     $('.b3').toggleClass('cinema');
     $('.button').toggleClass('hidden')
 
+  });
+
+  $('.b4').click(function(){
+    $('.b4').toggleClass('domiche');
+
+    // $('.specialfirst').toggleClass('hidden')
   });
 })
