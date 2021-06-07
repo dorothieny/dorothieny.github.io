ae09b64976924a63d83544fa0b075b73fb596b54
dorothieny <dorothinein@gmail.com>
2021-06-07 16:52:53 +0300

    kk

--- omyt/kode.js
+++ omyt/kode.js
@@ -55,7 +55,5 @@ $('.slider').slick({
    }
  ]
 });
-$('.buro').click(function(){
-  $(".")
-})
+
 });
---
+++ omyt/special.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.b1').click(function(){
+    $('.b1').toggleClass('boyboy');
+    $('.specialfirst').toggleClass('hidden')
+  });
+
+  $('.b3').click(function(){
+    $('.b3').toggleClass('cinema');
+    $('.button').toggleClass('hidden')
+
+  });
+})
