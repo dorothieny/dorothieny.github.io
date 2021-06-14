0be06db7b7ea0ffb77ace120100fc18002c233bc
dorothieny <dorothinein@gmail.com>
2021-06-14 03:08:04 +0300

    yay

--- omyt/special.js
+++ omyt/special.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  let rez = $('<div class="blank"></div>');
+    let rez = $('<div class="blank">  <form id="form" method="post"><div class=form1><label for="name"><h1>Имя:</h1></label><input type="text" name="name" placeholder="Ваше имя" id="name"></div><div class=form2><label for="phone"><h1>Телефон:</h1></label><input type="phone" name="phone" placeholder="+7" id="phone"></div><div class="form3"><span>Пол:</span><div class="male"><input type="radio" name="state" id="male inline" value="Мужской"><label id="inline"for="male">Мужской</label></div><div class="female"><input type="radio" name="state" id="female inline" value="Женский"><label  id="inline" for="male">Женский</label></div></div><div id="error" style="color: red"></div><input type="submit" name="submit" value="Готово"></form></div>');
 $('#zapis').click(function(){
 
     console.log("shto");
